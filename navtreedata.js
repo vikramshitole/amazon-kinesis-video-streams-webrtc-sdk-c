34c95903d7ed90e9f36314b8d9eb66bde802fa45
vikramshitole <vikramshitole@users.noreply.github.com>
2023-02-16 13:31:31 +0000

    Deploying to gh-pages from @ vikramshitole/amazon-kinesis-video-streams-webrtc-sdk-c@3f6850f132ea37769a117fc996763e633dbfb00b 🚀

---
+++ navtreedata.js
@@ -0,0 +1,43 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Amazon Kinesis Webrtc C SDK", "index.html", [
+    [ "Introduction", "md_Introduction.html", null ],
+    [ "Modules", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"group__Callbacks.html",
+"group__PublicEnums.html#ggad42c829d5095ba5e3ab0b6fff6ff8678afb72759d662497abc396c07ae5dea519",
+"group__StatusCodes.html#ga02a5e6d0b5cdd1916c3b6c7a295849d3",
+"group__StatusCodes.html#gafcb5243008cc0bf27f18df01e0a6ae6c",
+"structRtcReceivedRtpStreamStats.html#afdd919e3fd84bca61ba92e894633abb0"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['introduction_5038',['Introduction',['../md_Introduction.html',1,'']]]
+];
